0d398201bf2fde5021287b9a8a65f00a35c7619a
acramin <132026903+acramin@users.noreply.github.com>
2026-04-16 21:37:52 -0300

    Create HydroTwin.py
    pagina inicial da aplicação streamlit
    vai ser usada para descritivo e faq de como usar o site
    tem muito a ser feito aqui ainda

---
+++ HydroTwin.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import time
+
+st.set_page_config(page_title="Hydroponic Monitor", layout="wide", page_icon="🌱")
+
+st.title("🌱 HydroTwin")
+
+st.markdown(
+    """
+    Aqui pode ser uma descrição geral do projeto, explicando o que é o HydroTwin, seus objetivos e funcionalidades.
+    Explicar como que adiciona uma nova bancada, como que os dados são coletados e monitorados, e o que os usuários podem esperar do sistema.
+    Colocar também um link para o repositório do projeto no GitHub, para que os interessados possam acessar o código-fonte e contribuir.
+    Colocar imagens ilustrativas do sistema, como diagramas de arquitetura, fotos das bancadas, ou gráficos de monitoramento, para tornar a página mais visual e atrativa.
+    """
+)
