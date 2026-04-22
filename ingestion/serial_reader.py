ce8540d6039ed7fd0069ad65acca26badc469e33
acramin <132026903+acramin@users.noreply.github.com>
2026-04-22 17:39:53 -0300

    update serial_reader.py
    mudei de pasta
    verificando inserção no banco - apagar depois (já tem algo assim na main)

--- ingestion/serial_reader.py
+++ ingestion/serial_reader.py
@@ -2,6 +2,15 @@ import serial
 import sqlite3
 from datetime import datetime
 
+import sys
+from pathlib import Path
+
+# ROOT_DIR = Path(__file__).resolve().parents[1]
+# if str(ROOT_DIR) not in sys.path:
+#     sys.path.insert(0, str(ROOT_DIR))
+
+# from db.crud import processar_sensor
+
 PORTA = '/dev/ttyUSB0'  # pode mudar depois para a porta certa do arduino
 BAUD_RATE = 9600
 
@@ -17,10 +26,19 @@ def inserir_sensor_raw(bancada_id, ph, ec, temperatura_ambiente, temperatura_agu
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     """, (bancada_id, ph, ec, temperatura_ambiente, temperatura_agua, luminosidade, vazao, nivel_tanque, umidade, dth_recebido))
 
-    print("Dados inseridos no banco:", bancada_id, ph, ec, temperatura_ambiente, temperatura_agua, luminosidade, vazao, nivel_tanque, umidade, dth_recebido)
+    #print("Dados inseridos no banco:", bancada_id, ph, ec, temperatura_ambiente, temperatura_agua, luminosidade, vazao, nivel_tanque, umidade, dth_recebido)
+    error = cursor.execute("SELECT changes()").fetchone()[0]
+    if error == 0:
+        print("Erro ao inserir dados no banco!")
+    else:
+        print("Dados inseridos com sucesso!")
 
     conn.commit()
     conn.close()
+
+    # # Mantem score/status e alertas sempre atualizados apos nova leitura bruta.
+    # processar_sensor(bancada_id, janela_horaria="1min", horas=1/60)
+
 
 def parse_linha(linha):
     
