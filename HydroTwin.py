42c70169a397923aade428aaf880c3fa1d00a5f0
acramin <132026903+acramin@users.noreply.github.com>
2026-04-22 21:20:14 -0300

    Update HydroTwin.py
    por enquanto é uma página que vou usar para iniciar e parar o simulador
    assim não preciso rodar script toda hora

--- HydroTwin.py
+++ HydroTwin.py
@@ -1,7 +1,5 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import time
+from simulator.simulator import simular_dados, parar_simulacao
 
 st.set_page_config(page_title="Hydroponic Monitor", layout="wide", page_icon="🌱")
 
@@ -15,3 +13,14 @@ st.markdown(
     Colocar imagens ilustrativas do sistema, como diagramas de arquitetura, fotos das bancadas, ou gráficos de monitoramento, para tornar a página mais visual e atrativa.
     """
 )
+
+if st.button("Iniciar simulação de dados"):
+    simular_dados()
+    st.success(
+        "Simulação iniciada. O sistema agora insere dados falsos a cada 0.5 segundos e processa as bancadas ativas a cada 10 segundos."
+    )
+
+if st.button("Parar simulação de dados"):
+    parar_simulacao()
+    st.success("Simulação de dados parada.")
+    st.stop()
